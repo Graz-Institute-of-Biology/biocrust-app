f8f9f7ea6839e4402c768e5ded295ff7afe85489
Pacific89 <philipp.faulhammer@gmail.com>
2023-09-07 14:30:13 +0200

    clean start

---
+++ frontend/src/store/state.js
@@ -0,0 +1,3 @@
+export default {
+    data_set : ["empty data set"]
+}
---
+++ frontend/src/views/homePage.vue
@@ -0,0 +1,11 @@
+<template>
+    App-Home {{ data_set }}
+</template>
+
+<script setup>
+
+import { computed } from 'vue';
+import store from '../store';
+
+const data_set = computed(() => store.state.data_set)
+</script>
