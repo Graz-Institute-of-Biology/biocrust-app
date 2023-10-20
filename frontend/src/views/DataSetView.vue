0ec16e3842217624ac230cb81f58344768e85e23
Pacific89 <philipp.faulhammer@gmail.com>
2023-10-20 11:14:11 +0200

    added image gallery

--- frontend/src/views/DataSetView.vue
+++ frontend/src/views/DataSetView.vue
@@ -6,21 +6,11 @@
                 <RouterLink :to="{ name: 'AddImageView', params: { id: dataset.id }}" class="button is-link" v-if="!this.$store.loading">Add images</RouterLink>
             </div>
         </div>
-
-        <div class="columns is-multiline" v-if="!this.$store.loading">
-                <div
-                    class="column is-3"
-                    v-for="image in Images"
-                    :key=image.id
-                    >
-                    <div class="box">
-                        <h3 class="is-size-4"> {{ image.name }} </h3>
-                        <figure class="image-mb-4">
-                            <img :src="image.img" />
-                        </figure>
-                    </div>
-                </div>
+        <div  v-if="!this.$store.loading">
+        <div class="images" v-viewer="{movable: true}">
+            <img class="image-small" v-for="src in items" :src="src" :key="src">
             </div>
+        </div>
         <div v-else>
             <div class="columns is-multiline">
                 <div class="column is-12">
@@ -30,29 +20,44 @@
         </div>
     </div>
 
+
+
 </template>
 
 
 <script>
 import axios from 'axios'
+import { defineComponent } from 'vue'
+import 'viewerjs/dist/viewer.css'
+import { directive as viewer } from "v-viewer"
 
-export default {
+
+export default defineComponent({
     name: 'DataSetView',
-
+    directives: {
+      viewer: viewer({
+        debug: true
+      })
+    },
     data () {
         return {
             Images: [],
+            items: [],
             dataset: {},
-            dataset_name: this.$route.params.name
+            dataset_name: this.$route.params.name,
+            index: null
         }
     },
     created () {
+        this.$store.commit('setLoading', true)
         this.getDataset()
-        this.getImages()
     },
     methods: {
+        show () {
+        const viewer = this.$el.querySelector('.images').$viewer
+        viewer.show()
+      },
         async getDataset() {
-            this.$store.commit('setLoading', true)
             await axios.get(`api/v1/datasets/${this.$route.params.id}`)
             .then(response => {
                 this.dataset = response.data
@@ -60,17 +65,23 @@ export default {
             .catch(error => {
                 console.log(error)
             })
-            this.$store.commit('setLoading', false)
+            console.log("Dataset loaded")
+            this.getImages()
     },
         async getImages() {
-                this.$store.commit('setLoading', true)
                 await axios.get('api/v1/images/')
                 .then(response => {
                     this.Images = response.data.filter(image => image.dataset == this.$route.params.id)
+                    let img_items = response.data.filter(image => image.dataset == this.$route.params.id)
+                    for (let i = 0; i < img_items.length; i++) {
+                        this.items.push(img_items[i].img)
+                    }
+                    console.log(this.items)
                 })
                 .catch(error => {
                     console.log(error)
                 })
+                console.log("Images loaded")
                 this.$store.commit('setLoading', false)
             },
         getUrl(image) {
@@ -78,7 +89,7 @@ export default {
             return url
         }
     }
-}
+})
 
 
 </script>
@@ -86,5 +97,18 @@ export default {
 
 
 <style scoped>
+.page-dataset {
+    margin-bottom: 10%;
+}
+.image-small {
+    height: 200px;
+    cursor:pointer;
+    margin: 15px;
+    display: inline-block;
+    transition: .1s linear;
+}
+.image-small:hover {
+    transform: scale(1.1);
+}
 
 </style>
