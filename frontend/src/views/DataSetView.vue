15293ab4e1cd52310f9d8572075c0a0982bf83b5
Pacific89 <philipp.faulhammer@gmail.com>
2023-10-20 12:29:19 +0200

    add custom options

--- frontend/src/views/DataSetView.vue
+++ frontend/src/views/DataSetView.vue
@@ -4,10 +4,11 @@
             <div class="column is-12">
                 <h1 class="title is-1">{{ dataset.dataset_name }}</h1>
                 <RouterLink :to="{ name: 'AddImageView', params: { id: dataset.id }}" class="button is-link" v-if="!this.$store.loading">Add images</RouterLink>
+                <div class="button is-success" @click="analyze" v-if="!this.$store.loading">Analyze images</div>
             </div>
         </div>
         <div  v-if="!this.$store.loading">
-        <div class="images" v-viewer="{movable: true}">
+        <div class="images" v-viewer="options">
             <img class="image-small" v-for="src in items" :src="src" :key="src">
             </div>
         </div>
@@ -31,21 +32,44 @@ import { defineComponent } from 'vue'
 import 'viewerjs/dist/viewer.css'
 import { directive as viewer } from "v-viewer"
 
-
 export default defineComponent({
     name: 'DataSetView',
     directives: {
       viewer: viewer({
         debug: true
       })
-    },
+ },
     data () {
         return {
             Images: [],
             items: [],
             dataset: {},
             dataset_name: this.$route.params.name,
-            index: null
+            index: null,
+            options: {
+                        ready: () => {
+                        this.$viewer = this.$el.querySelector(".images").$viewer;
+                        },
+                        inline: false,
+                        toolbar: {
+                                    oneToOne: true,
+                                    movable: true,
+                                    prev: () => {
+                                    this.$viewer.prev(true);
+                                    },
+                                    play: false,
+                                    next: () => {
+                                    this.$viewer.next(true);
+                                    },
+                                    overlay: () => {
+                                        const viewer = this.$viewer;
+                                        const a = document.createElement("a");
+                                        a.href = viewer.image.src;
+                                        a.download = viewer.image.alt;
+                                        
+                                    }
+                                },
+   },
         }
     },
     created () {
@@ -101,7 +125,7 @@ export default defineComponent({
     margin-bottom: 10%;
 }
 .image-small {
-    height: 200px;
+    height: 500px;
     cursor:pointer;
     margin: 15px;
     display: inline-block;
@@ -110,5 +134,18 @@ export default defineComponent({
 .image-small:hover {
     transform: scale(1.1);
 }
+.viewer-overlay::before {
+  content: "download";
+  color: transparent;
+  display: block;
+  font-size: 0;
+  height: 20px;
+  line-height: 0;
+  width: 20px;
+  background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAAXNSR0IArs4c6QAAAXBJREFUOE/VlDFLlmEUhq9ra3HoT0RDCOIQEQnVkJRQU4QQJEKBICKmNFWEODRHUxJEaDT1ExxUJFLQpSEcxJ+gIE13PPa9H6+f72cSLr7j85xz3ec+57yPnPHnGfM4x8Ak88AD4ADYVO+dpj2NlpP8AH4CM0APMAX0A88rqLrUJHAMmGQWuK7erCckmQOutc76gA9qW6At1KmS5COwrr47yWKStIRX63FHKkxSlN8CL9T1fwAnChCYVbeOVZjkPvAZWFCfddi9AYyrDzvOR4t1YFktMX/3MMkVYBF40lRZkkZgBU/yFdhRpyvgBvBG/dZkswV8rD7tcj8EjKl3K+AucEv91SVhABhRR7rcvwQuqpMVcBIYVO909OgL8B0oQo+AfeBSfaWS9JbFB4bVxfaUk6wAe8Ca+rrWn/fAVWD7sOm1wST5BNwGXqllOEcfhySlist1YGtoJfFCw5THgHn1d9fFPs3/elLMOX6+/tf6HzsPjRVd/9/RAAAAAElFTkSuQmCC);
+}
 
+.button {
+    margin-right: 10px;
+}
 </style>
