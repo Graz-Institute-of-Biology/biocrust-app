73dbee4216d4bca103574cb12fccb658f3d7db3b
Stefan Herdy <69188729+stefanherdy@users.noreply.github.com>
2023-10-22 11:13:11 +0200

    add about cards

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -5,8 +5,158 @@
             <p> About this project some example text</p>
         </section>
     </div>
-  </template>
+    <div class="container has-text-centered">
+        <header class="container">
+        <h1>About Us</h1>
+        <img src="@/assets/logo.jpg" alt="Work Group" class="container" />
+        </header>
 
-<script setup>
+        <div class="container">
+        <p>{{ description }}</p>d
+        </div>
 
+        <ul class="social-links">
+        <li v-for="socialLink in socialLinks" :key="socialLink.name">
+            <a :href="socialLink.url" target="_blank">{{ socialLink.name }}</a>
+        </li>
+        </ul>
+
+        <h2 style="text-align:center">Our Team</h2>
+        <div class="center-vertically">
+        <div class="column">
+            <div class="card">
+            <img src="@/assets/logo.jpg" alt="Stefan Herdy" style="width:100%">
+            <div class="container">
+                <h2>Stefan Herdy</h2>
+                <p class="title">Co-Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>stefan.herdy@uni-graz.at</p>
+                <p><button class="button">Contact</button></p>
+            </div>
+            </div>
+        </div>
+
+
+        <div class="column">
+            <div class="card">
+            <img src="@/assets/logo.jpg" alt="Philipp Faulhammer" style="width:100%">
+            <div class="container">
+                <h2>Philipp Faulhammer</h2>
+                <p class="title">Co-Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>philipp.faulhammer@uni-graz.at</p>
+                <p><button class="button">Contact</button></p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      workGroupImage: "work-group-image.jpg",
+      description: "Welcome to our website! We are a passionate group of individuals dedicated to excellence in our field. Our mission is to provide the best solutions for your needs.",
+      socialLinks: [
+        { name: "GitHub", url: "https://github.com/Graz-Institute-of-Biology" },
+      ],
+      formData: {
+        name: "",
+        email: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission, e.g., send data to the server
+      console.log("Form submitted with data:", this.formData);
+    },
+  },
+};
 </script>
+
+<style scoped>
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.center-vertically {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+}
+
+.column {
+  float: left;
+  width: 30%;
+  margin: auto;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  margin: 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after, .row::after {
+  margin: auto;
+  width: 100%;
+  padding: 10px;
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+</style>
