99aa533ce238b2723c3889deaa43c81c8557055b
pacific89 <philipp.faulhammer@gmail.com>
2024-02-06 09:21:38 +0100

    add timeout for testing

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,9 @@ else:
                  {
                     'ENGINE': 'django.db.backends.sqlite3',
                     'NAME': BASE_DIR / 'db.sqlite3',
+                    'OPTIONS': {
+                                    'timeout': 20,  # in seconds
+                                }
         }
     }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
