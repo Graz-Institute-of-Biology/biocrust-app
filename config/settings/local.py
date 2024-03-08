e398f201b11aa40e1d65ab4745b4e7547573f266
Pacific89 <philipp.faulhammer@gmail.com>
2024-03-08 13:42:23 +0100

    add host

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="8OmLatOrLmWP8ujI6JiuIxGhfjGB87ULLpWxwzvY03Jr50bMs9wjEdgyKTNDlVJV",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "167.99.251.188"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "167.99.251.188", "cc-explorer.com"]
 # ALLOWED_HOSTS = ["167.99.251.188"]
 
 CORS_ALLOWED_ORIGINS = [
