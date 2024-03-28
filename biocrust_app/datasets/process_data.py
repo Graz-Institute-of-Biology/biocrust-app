2855ae3e6682d354a7bd12b0fae60c3a8488d709
stefanherdy <stefan.herdy@gmail.com>
2024-03-28 13:34:42 +0100

    fix color upload

--- biocrust_app/datasets/process_data.py
+++ biocrust_app/datasets/process_data.py
@@ -31,20 +31,18 @@ def generate_class_dist(image, dataset_type):
     for class_label in color_config:
         class_counts[color_config[class_label]['name']] = 0
     print('Class Counts: ', class_counts)
-    if len(image.size) == 3:
+    print(image.size)
+    if image.mode == "RGB":
         for i in range(image.size[0]):
             for j in range(image.size[1]):
                 pixel_value = pixels[i, j]
                 class_label = pixel_to_label.get(pixel_value)
                 if class_label:
                     class_counts[class_label] += 1
-    if len(image.size) == 2:
+    if image.mode == "L":
         for i in range(image.size[0]):
             for j in range(image.size[1]):
-                #print(color_config)
-                #print(pixels[i, j])
                 class_label = color_config[str(pixels[i, j])]['name']
-                #print(class_label)
                 class_counts[class_label] += 1
 
     total_pixels = sum(class_counts.values())
@@ -66,18 +64,16 @@ def get_color(category):
 
 def translate_categorical_to_color(input_image, dataset_type):
     pixels = input_image.load()
-    if len(input_image.size) == 2:
-        print('Translating categorical to color')
-        colored_image = np.zeros((input_image.size[1], input_image.size[0], 3), dtype=np.uint8) 
-        ontology_path = './biocrust_app/datasets/{0}_ontology.json'.format(dataset_type)
-        with open(ontology_path, 'r') as f:
-            ontology = json.load(f)
-        
-        for i in range(input_image.size[0]):
-            for j in range(input_image.size[1]):
-                color = ontology.get(str(pixels[i, j]), {}).get('color', [0, 0, 0])
-                colored_image[j, i, :] = color
-        return input_image, colored_image
-    else:
-        return input_image, pixels
+    
+    print('Translating categorical to color')
+    colored_image = np.zeros((input_image.size[1], input_image.size[0], 3), dtype=np.uint8) 
+    ontology_path = './biocrust_app/datasets/{0}_ontology.json'.format(dataset_type)
+    with open(ontology_path, 'r') as f:
+        ontology = json.load(f)
+    
+    for i in range(input_image.size[0]):
+        for j in range(input_image.size[1]):
+            color = ontology.get(str(pixels[i, j]), {}).get('color', [0, 0, 0])
+            colored_image[j, i, :] = color
+    return input_image, colored_image
 
