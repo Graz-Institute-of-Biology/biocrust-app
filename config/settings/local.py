b0eace270580c69eda1978fb4bb6180cec9de44e
Pacific89 <philipp.faulhammer@gmail.com>
2024-12-16 13:21:18 +0100

    test

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,9 +12,9 @@ SECRET_KEY = env(
     default="8OmLatOrLmWP8ujI6JiuIxGhfjGB87ULLpWxwzvY03Jr50bMs9wjEdgyKTNDlVJV",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "django"]
 # ALLOWED_HOSTS = ["167.99.251.188"]
-ALLOWED_HOSTS = ["cc-explorer.com", "api.cc-explorer.com", "167.99.251.188", "django"]
+# ALLOWED_HOSTS = ["cc-explorer.com", "api.cc-explorer.com", "167.99.251.188", "django"]
 
 
 CORS_ALLOWED_ORIGINS = [
@@ -57,7 +57,7 @@ DOMAIN = "cc-explorer.com"
 
 
 DJOSER = {
-    'SEND_ACTIVATION_EMAIL' : True,
+    'SEND_ACTIVATION_EMAIL' : False,
     'ACTIVATION_URL': 'activate/{uid}/{token}',
 }
 
