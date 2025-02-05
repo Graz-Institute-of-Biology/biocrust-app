e4b440c20f4b0a7a0fe0fc6fbfc6fc2c95062720
pacific89 <philipp.faulhammer@gmail.com>
2025-02-05 11:58:59 +0100

    up

--- config/settings/base.py
+++ config/settings/base.py
@@ -176,7 +176,7 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
 MEDIA_ROOT = str(APPS_DIR / "media")
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
-MEDIA_URL = "django/media/"
+MEDIA_URL = "media/"
 
 # TEMPLATES
 # ------------------------------------------------------------------------------
